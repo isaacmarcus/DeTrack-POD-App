02a41be9f9db3b83a38f530c22710315b0ebc40e
Isaac Marcus <32948643+isaacmarcus@users.noreply.github.com>
2021-11-11 18:18:58 +0800

    moved download card to a seperate file

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:detrack_pod_dl_app/constants.dart';
+import 'package:detrack_pod_dl_app/widgets/download_card.dart';
 import 'package:detrack_pod_dl_app/widgets/menu_drawer.dart';
 import 'package:detrack_pod_dl_app/widgets/master_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -33,11 +34,6 @@ class _LandingPageState extends State<LandingPage>
       vsync: this,
       duration: const Duration(milliseconds: 150),
     );
-
-    // Initialize today's date
-    _range = DateFormat('dd/MM/yyyy').format(DateTime.now()).toString() +
-        ' - ' +
-        DateFormat('dd/MM/yyyy').format(DateTime.now()).toString();
   }
 
   @override
@@ -49,45 +45,6 @@ class _LandingPageState extends State<LandingPage>
   // function to check on the menu drawer, only applies in mobile size
   bool _isDrawerClosed() {
     return _drawerSlideController.value == 0.0;
-  }
-
-  String _selectedDate = '';
-  String _dateCount = '';
-  String _range = '';
-  String _rangeCount = '';
-  String _apiKey = "4840603d74969363341bd6db9637d635971369c873959bd5";
-
-  /// The method for [DateRangePickerSelectionChanged] callback, which will be
-  /// called whenever a selection changed on the date picker widget.
-  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
-    /// The argument value will return the changed date as [DateTime] when the
-    /// widget [SfDateRangeSelectionMode] set as single.
-    ///
-    /// The argument value will return the changed dates as [List<DateTime>]
-    /// when the widget [SfDateRangeSelectionMode] set as multiple.
-    ///
-    /// The argument value will return the changed range as [PickerDateRange]
-    /// when the widget [SfDateRangeSelectionMode] set as range.
-    ///
-    /// The argument value will return the changed ranges as
-    /// [List<PickerDateRange] when the widget [SfDateRangeSelectionMode] set as
-    /// multi range.
-    setState(() {
-      if (args.value is PickerDateRange) {
-        _range =
-            DateFormat('dd/MM/yyyy').format(args.value.startDate).toString() +
-                ' - ' +
-                DateFormat('dd/MM/yyyy')
-                    .format(args.value.endDate ?? args.value.startDate)
-                    .toString();
-      } else if (args.value is DateTime) {
-        _selectedDate = args.value.toString();
-      } else if (args.value is List<DateTime>) {
-        _dateCount = args.value.length.toString();
-      } else {
-        _rangeCount = args.value.length.toString();
-      }
-    });
   }
 
   // ** Main build widget for the Page **
@@ -130,138 +87,7 @@ class _LandingPageState extends State<LandingPage>
         children: <Widget>[
           Center(
             // CARD WIDGET FOR COLLECTIONS DOWNLOADER
-            // *TODO* move to widgets folder
-            child: Container(
-              width: MediaQuery.of(context).size.width >= 725
-                  ? 385
-                  : double.infinity,
-              child: Card(
-                color: themeData.cardColor,
-                elevation: 5.0,
-                child: Padding(
-                  padding: kCardPaddingL,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Collections",
-                                style: themeData.textTheme.headline3,
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              // Text(
-                              //   'Selected date: ' + _selectedDate,
-                              //   style: themeData.textTheme.subtitle1,
-                              // ),
-                              // Text(
-                              //   'Selected date count: ' + _dateCount,
-                              //   style: themeData.textTheme.subtitle1,
-                              // ),
-                              Text(
-                                'Selected range: ' + _range,
-                                style: themeData.textTheme.subtitle1,
-                              ),
-                              // Text(
-                              //   'Selected ranges count: ' + _rangeCount,
-                              //   style: themeData.textTheme.subtitle1,
-                              // ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      // MAIN Date Picker Widget
-                      Container(
-                        margin: EdgeInsets.zero,
-                        width: double.infinity,
-                        child: Card(
-                          margin: EdgeInsets.zero,
-                          color: Color(0xFF340909),
-                          elevation: 5,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              // Container to hold Date Range Picker
-                              Container(
-                                margin: EdgeInsets.only(top: 10),
-                                width: MediaQuery.of(context).size.width >= 725
-                                    ? 300
-                                    : MediaQuery.of(context).size.width * 0.75,
-                                child: SfDateRangePicker(
-                                  // Styling for the date picker
-                                  headerHeight: 50,
-                                  selectionTextStyle:
-                                      themeData.textTheme.headline6,
-                                  rangeTextStyle: themeData.textTheme.subtitle1,
-                                  headerStyle: DateRangePickerHeaderStyle(
-                                      textStyle: themeData.textTheme.headline4),
-                                  monthViewSettings:
-                                      DateRangePickerMonthViewSettings(
-                                    viewHeaderStyle:
-                                        DateRangePickerViewHeaderStyle(
-                                            textStyle:
-                                                themeData.textTheme.headline5),
-                                  ),
-                                  monthCellStyle: DateRangePickerMonthCellStyle(
-                                    textStyle: themeData.textTheme.subtitle1,
-                                    todayTextStyle:
-                                        themeData.textTheme.subtitle1,
-                                  ),
-                                  yearCellStyle: DateRangePickerYearCellStyle(
-                                    textStyle: themeData.textTheme.subtitle1,
-                                    disabledDatesTextStyle:
-                                        themeData.textTheme.subtitle2,
-                                    todayTextStyle:
-                                        themeData.textTheme.subtitle1,
-                                    leadingDatesTextStyle:
-                                        themeData.textTheme.subtitle2,
-                                  ),
-                                  viewSpacing: 5,
-                                  onSelectionChanged: _onSelectionChanged,
-                                  selectionMode:
-                                      DateRangePickerSelectionMode.range,
-                                  initialDisplayDate: DateTime.now(),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      ElevatedButton(
-                        onPressed: () {
-                          print("test");
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "Download",
-                            style: themeData.textTheme.button,
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: Size(
-                            double.infinity,
-                            30,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            child: DownloadCard(),
           ),
         ],
       ),
---
+++ lib/widgets/download_card.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+import '../constants.dart';
+
+class DownloadCard extends StatefulWidget {
+  const DownloadCard({Key? key}) : super(key: key);
+
+  @override
+  _DownloadCardState createState() => _DownloadCardState();
+}
+
+class _DownloadCardState extends State<DownloadCard> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize today's date
+    _range = DateFormat('dd/MM/yyyy').format(DateTime.now()).toString() +
+        ' - ' +
+        DateFormat('dd/MM/yyyy').format(DateTime.now()).toString();
+  }
+
+  String _selectedDate = '';
+  String _dateCount = '';
+  String _range = '';
+  String _rangeCount = '';
+  String _apiKey = "4840603d74969363341bd6db9637d635971369c873959bd5";
+
+  /// The method for [DateRangePickerSelectionChanged] callback, which will be
+  /// called whenever a selection changed on the date picker widget.
+  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
+    /// The argument value will return the changed date as [DateTime] when the
+    /// widget [SfDateRangeSelectionMode] set as single.
+    ///
+    /// The argument value will return the changed dates as [List<DateTime>]
+    /// when the widget [SfDateRangeSelectionMode] set as multiple.
+    ///
+    /// The argument value will return the changed range as [PickerDateRange]
+    /// when the widget [SfDateRangeSelectionMode] set as range.
+    ///
+    /// The argument value will return the changed ranges as
+    /// [List<PickerDateRange] when the widget [SfDateRangeSelectionMode] set as
+    /// multi range.
+    setState(() {
+      if (args.value is PickerDateRange) {
+        _range =
+            DateFormat('dd/MM/yyyy').format(args.value.startDate).toString() +
+                ' - ' +
+                DateFormat('dd/MM/yyyy')
+                    .format(args.value.endDate ?? args.value.startDate)
+                    .toString();
+      } else if (args.value is DateTime) {
+        _selectedDate = args.value.toString();
+      } else if (args.value is List<DateTime>) {
+        _dateCount = args.value.length.toString();
+      } else {
+        _rangeCount = args.value.length.toString();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width >= 725 ? 385 : double.infinity,
+      child: Card(
+        color: themeData.cardColor,
+        elevation: 5.0,
+        child: Padding(
+          padding: kCardPaddingL,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Collections",
+                        style: themeData.textTheme.headline3,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Selected range: ' + _range,
+                        style: themeData.textTheme.subtitle1,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              // MAIN Date Picker Widget
+              Container(
+                margin: EdgeInsets.zero,
+                width: double.infinity,
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  color: Color(0xFF340909),
+                  elevation: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      // Container to hold Date Range Picker
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        width: MediaQuery.of(context).size.width >= 725
+                            ? 300
+                            : MediaQuery.of(context).size.width * 0.75,
+                        child: SfDateRangePicker(
+                          // Styling for the date picker
+                          headerHeight: 50,
+                          selectionTextStyle: themeData.textTheme.headline6,
+                          rangeTextStyle: themeData.textTheme.subtitle1,
+                          headerStyle: DateRangePickerHeaderStyle(
+                              textStyle: themeData.textTheme.headline4),
+                          monthViewSettings: DateRangePickerMonthViewSettings(
+                            viewHeaderStyle: DateRangePickerViewHeaderStyle(
+                                textStyle: themeData.textTheme.headline5),
+                          ),
+                          monthCellStyle: DateRangePickerMonthCellStyle(
+                            textStyle: themeData.textTheme.subtitle1,
+                            todayTextStyle: themeData.textTheme.subtitle1,
+                          ),
+                          yearCellStyle: DateRangePickerYearCellStyle(
+                            textStyle: themeData.textTheme.subtitle1,
+                            disabledDatesTextStyle:
+                                themeData.textTheme.subtitle2,
+                            todayTextStyle: themeData.textTheme.subtitle1,
+                            leadingDatesTextStyle:
+                                themeData.textTheme.subtitle2,
+                          ),
+                          viewSpacing: 5,
+                          onSelectionChanged: _onSelectionChanged,
+                          selectionMode: DateRangePickerSelectionMode.range,
+                          initialDisplayDate: DateTime.now(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  print("test");
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Download",
+                    style: themeData.textTheme.button,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(
+                    double.infinity,
+                    30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
